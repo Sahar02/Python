e591721686e4d9cef1905b90a881d04c658bb511
Sahar02 <SHAHEEDSARA2@GMAIL.COM>
2022-06-27 17:47:00 -0400

    Create functions_basic_i.py

---
+++ Fundementals/Practice/functions_basic_i.py
@@ -0,0 +1,170 @@
+#1
+# def number_of_food_groups():
+#     return 5
+# print(number_of_food_groups())
+# Predicted output > 5
+
+
+#2
+# def number_of_military_branches():
+#     return 5
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# Predicted output > 10
+
+
+#3
+# def number_of_books_on_hold():
+#     return 5
+#     return 10
+# print(number_of_books_on_hold())
+# Predicted output > 10
+
+
+#4
+# def number_of_fingers():
+#     return 5
+#     print(10)
+# print(number_of_fingers())
+# Predicted output > 10
+# Predicted output > 5
+
+
+
+#5
+# def number_of_great_lakes():
+#     print(5)
+# x = number_of_great_lakes()
+# print(x)
+# Predicted output > 5
+# Predicted output > None
+
+
+#6
+# def add(b,c):
+#     print(b+c)
+# print(add(1,2) + add(2,3))
+# Predicted output > 1,2
+# # Predicted output > 2,3
+# # Predicted output >8
+
+
+#7
+# def concatenate(b,c):
+#     return str(b)+str(c)
+# print(concatenate(2,5))
+# Predicted output > 2,5
+
+
+#8
+# def number_of_oceans_or_fingers_or_continents():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(number_of_oceans_or_fingers_or_continents())
+# Predicted output > 100
+# Predicted output > 10
+# Predicted output > 7
+
+
+#9
+# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# Predicted output > 7,3
+# Predicted output > 14, 3
+# Predicted output > 16
+
+#10
+# def addition(b,c):
+#     return b+c
+#     return 10
+# print(addition(3,5))
+# Predicted output > 8,10
+
+
+#11
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+# print(b)
+# foobar()
+# print(b)
+# Predicted output > 500
+# Predicted output > 300
+# Predicted output > 500
+# Predicted output > none
+# Predicted output > 500
+
+
+#12
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# foobar()
+# print(b)
+# Predicted output > 500
+# Predicted output > 300
+# Predicted output > 500
+# Predicted output > 300
+# Predicted output > 500
+
+#13
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=foobar()
+# print(b)
+# Predicted output > 500
+# Predicted output > 300
+# Predicted output > 500
+# Predicted output > 500
+
+
+#14
+# def foo():
+#     print(1)
+#     bar()
+#     print(2)
+# def bar():
+#     print(3)
+# foo()
+# Predicted output > 1
+# Predicted output > 2
+# Predicted output > 3
+
+
+#15
+# def foo():
+#     print(1)
+#     x = bar()
+#     print(x)
+#     return 10
+# def bar():
+#     print(3)
+#     return 5
+# y = foo()
+# print(y)
+# Predicted output > 1
+# Predicted output > 10
+# Predicted output > 3
+# Predicted output > 5
