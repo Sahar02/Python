2e597a40883f630a5e4b1a2c77700fc9c3ae4882
Sahar02 <SHAHEEDSARA2@GMAIL.COM>
2022-07-13 17:35:46 -0400

    Update UserWithBankAccounts.py

--- Fundementals/Assignments/UserWithBankAccounts.py
+++ Fundementals/Assignments/UserWithBankAccounts.py
@@ -3,7 +3,7 @@ class BankAccount:
     
     all_accounts = []
     # don't forget to add some default values for these parameters!
-    def __init__(self, int_rate, balance): 
+    def __init__(self,int_rate, balance): 
         self.int_Rate =int_rate
         self.balance = balance
         # don't worry about user info here; we'll involve the User class soon
@@ -22,7 +22,7 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Balance {self.balance}")
+        print(f"\n Balance {self.balance}")
         return self
 
     def yield_interest(self):
@@ -30,33 +30,56 @@ class BankAccount:
             self.balance+=self.int_Rate
         return self
 
+    def  transfer_money(self, amount,user):
+        self.balance-=amount
+        user.account.balance+=amount
+
     @classmethod
     def BankAccount_summary(cls):
         sum = 0
         for account in cls.all_accounts:
-            sum+=account
-            print(f"Balance: {sum}")
+            sum=account
+            print(f"\n Balance: {sum}")
         return sum
 
 
-class User(BankAccount):
+# We can now access the BankAccount class through our self.account attribute
+class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.account = BankAccount(int_rate=0.01, balance=0)
+        self.account = BankAccount(int_rate=0.02, balance=0)
 
-    def deposit(self, amount):
-        self.account+=amount
+    def deposit(self,amount):
+        self.balance+=amount
         return self
-
+    
     def withdraw(self, amount):
-        if amount < self.account:
-            self.account-=amount
+        if amount < self.balance:
+            self.balance-=amount
         else:
             print(f"Insufficient funds: Charging a $5 fee")
-            self.account-=2
+            self.balance-=2
+        return self
+    
+    def display_account_info(self):
+        print(f"\n Balance {self.balance}")
         return self
 
-    def display_account_info(self):
-        print(f"\nBalance {self.account}")
-        return self
+    def  transfer_money(self, amount,user):
+        self.account.balance-=amount
+        user.account.balance+=amount
+
+Mike = User('Mike','MikeL@codingdojo.com')
+Mike.account.deposit(50)
+Mike.account.display_account_info()
+
+Sara = User('Sara','SaraB@codingdojo.com')
+Sara.account.deposit(10)
+Sara.account.display_account_info()
+
+# Money transfer
+Mike.account.transfer_money(5,Sara)
+Mike.account.display_account_info()
+
+Sara.account.display_account_info()
